7f8fc5f84e099b172a31693421086f58888be714
boredherobrine13 <treeclan12@gmail.com>
2016-03-22 13:50:10 -0400

    Added TNT.

--- src/main/java/com/bored/morefuelsmod/Fuels.java
+++ src/main/java/com/bored/morefuelsmod/Fuels.java
@@ -155,6 +155,10 @@ public class Fuels implements IFuelHandler{
 				//Vanilla chest burns for 300 ticks.
 					return 400;
 				}
+				//Adding TNT
+				if (fuel.getItem() == Item.getItemFromBlock(Blocks.tnt)){
+					return 8080;
+				}
 				return 0;
 			}
 		}
